90f5cacd71642839af81b9f4451e4856022445ab
PapoMage <pablo.fernandez1@davinci.edu.ar>
2017-08-29 12:02:44 -0300

    Creacion de la entidad User + BD

--- src/Papo/UserBundle/Controller/UserController.php
+++ src/Papo/UserBundle/Controller/UserController.php
@@ -9,7 +9,25 @@ class UserController extends Controller
 {
     public function indexAction()
     {
-        return new Response('Welcome to the user module');
+        $em = $this->getDoctrine()->getManager(); //Permite acceder a los metodos y servicios de Doctrine
+
+        $users = $em->getRepository('PapoUserBundle:User')->findAll(); //Permite acceder a todos los registros de la base de datos.
+        $res = 'Lista de usuarios: <br />';
+
+        foreach ($users as $user) {
+        	$res .= 'Usuario: '. $user->getUsername(). ' - Email: '. $user->getEmail(). '<br />';
+        }
+
+        return new Response ($res);
+    }
+
+    public function viewAction($id)
+    {
+    	$repository = $this->getDoctrine()->getRepository('PapoUserBundle:User');
+
+    	$user = $repository->find($id);
+
+    	return new Response ('Usuario: '. $user->getUsername(). ' - Su email es: '. $user->getEmail());
     }
 
     public function articlesAction($page)
